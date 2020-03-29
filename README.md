174e19670daabf289ad51f308a66d151dd11d01b
zomry1 <zomry1@gmail.com>
2020-03-30 00:43:24 +0300

    Fix files names, add coments, fix vars names

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # UltimateDecreptor
  
+Sainty by 3 things:
+1) api of english words
+2) flag format
+3) Frequency analysis - Done
+
+verbose option
+show precent of sentiy
+
+Codecs:
+Reverse - Done
+Vigenere
+Shift (ROT - Ceasar) - Done
+Flip keypad
+Md5 (known hash)
+base64 - Done
+all bases
+Ascii - Done
+URL encode
+
+
+
+
+More:
+find == at the end so it's base64
+Frequency analysis
+extract mails or ips
+strings
+file
+
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from CeasarCipher import CaesarDecreptor
-from SantiyCheck import Decoder
-ciphertext = 'Up fodpef tusjoht dpoubjojoh tqbdft boe vqqfs dbtf mfuufst, vtf uif cvjmu-jo dpefdt mjcsbsz.'
-ciphertext1 = """Creuncf gur zbfg jryy-choyvpvmrq grpu gbby va Ehffvn'f nefrany sbe svtugvat pbebanivehf vf Zbfpbj'f znffvir snpvny-erpbtavgvba flfgrz. Ebyyrq bhg rneyvre guvf lrne, gur fheirvyynapr flfgrz unq bevtvanyyl cebzcgrq na hahfhny choyvp onpxynfu, jvgu cevinpl nqibpngrf svyvat ynjfhvgf bire haynjshy fheirvyynapr.
+from CaesarCipher import CaesarDecreptor
+from SanityCheck import evaluation
+
+ciphertext = """Creuncf gur zbfg jryy-choyvpvmrq grpu gbby va Ehffvn'f nefrany sbe svtugvat pbebanivehf vf Zbfpbj'f znffvir snpvny-erpbtavgvba flfgrz. Ebyyrq bhg rneyvre guvf lrne, gur fheirvyynapr flfgrz unq bevtvanyyl cebzcgrq na hahfhny choyvp onpxynfu, jvgu cevinpl nqibpngrf svyvat ynjfhvgf bire haynjshy fheirvyynapr.
 Pbebanivehf, ubjrire, unf tvira na harkcrpgrq choyvp-eryngvbaf obbfg gb gur flfgrz.
 Ynfg jrrx, Zbfpbj cbyvpr pynvzrq gb unir pnhtug naq svarq 200 crbcyr jub ivbyngrq dhnenagvar naq frys-vfbyngvba hfvat snpvny erpbtavgvba naq n 170,000-pnzren flfgrz. Nppbeqvat gb n Ehffvna zrqvn ercbeg fbzr bs gur nyyrtrq ivbyngbef jub jrer svarq unq orra bhgfvqr sbe yrff guna unys n zvahgr orsber gurl jrer cvpxrq hc ol n pnzren.
 "Jr jnag gurer gb or rira zber pnzrenf fb gung gung gurer vf ab qnex pbeare be fvqr fgerrg yrsg," Byrt Onenabi, Zbfpbj'f cbyvpr puvrs, fnvq va n erprag oevrsvat, nqqvat gung gur freivpr vf pheeragyl jbexvat gb vafgnyy na nqqvgvbany 9,000 pnzrenf."""
-plaintexts = CaesarDecreptor(ciphertext1)
+plaintexts = CaesarDecreptor(ciphertext)
 
-answers = []
+evaluations = []
+#Evaluate each plaintext and calculate error
 for plaintext in plaintexts:
-	answers.append((plaintext[:20], Decoder(plaintext)))
+	evaluations.append((plaintext[:20], evaluation(plaintext)))
 
-answers = sorted(answers, key=lambda x: x[1])
-print('\n'.join([str(element) for element in answers]))
+#Sort plaintext by error level
+evaluations = sorted(evaluations, key=lambda x: x[1])
+#Print summery of each plaintext and error
+print('\n'.join([str(element) for element in evaluations]))
