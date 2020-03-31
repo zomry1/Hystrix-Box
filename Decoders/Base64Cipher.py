e8ff83e84922522935bf30d6fd171f3fa093221a
zomry1 <zomry1@gmail.com>
2020-03-31 18:01:54 +0300

    Fix Base64 decoder
    If text is not 64base decoder return empty list

--- Decoders/Base64Cipher.py
+++ Decoders/Base64Cipher.py
@@ -2,4 +2,10 @@ import base64
 
 
 def Base64Decoder(ciphertext):
-    return base64.decodebytes(ciphertext)
+    try:
+        base64_bytes = ciphertext.encode('ascii')
+        message_bytes = base64.b64decode(base64_bytes)
+        ciphertext = message_bytes.decode('ascii')
+        return [ciphertext]
+    except:
+        return []
