d531bceee24229658c09cb38fa47055baa6ccaad
zomry1 <zomry1@gmail.com>
2020-03-31 17:50:13 +0300

    Fix ASCIIDecoder
    If not all the ciphertext is numbers return empty list

--- Decoders/ASCIICipher.py
+++ Decoders/ASCIICipher.py
@@ -1,2 +1,6 @@
 def ASCIIDecoder(ciphertext):
-	return ''.join(chr(int(char)) for char in ciphertext.split())
+	#If not all the ciphertext is numbers return empty list
+    if all(isinstance(part, int) for part in ciphertext.split()):
+	    return ''.join(chr(int(char)) for char in list(ciphertext))
+    else:
+        return []
