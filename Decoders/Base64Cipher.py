38ea8b493ec2b906595ac059d97a9f30155286e6
zomry1 <zomry1@gmail.com>
2020-04-05 15:00:03 +0300

    Use 64base validator

--- Decoders/Base64Cipher.py
+++ Decoders/Base64Cipher.py
@@ -1,7 +1,11 @@
 import base64
+from Utils.base64Validator import checkBase64Format
 
 
 def Base64Decoder(ciphertext):
+    # Check if ciphertext is 64base format
+    if not checkBase64Format(ciphertext):
+        return []
     try:
         base64_bytes = ciphertext.encode('ascii')
         message_bytes = base64.b64decode(base64_bytes)
