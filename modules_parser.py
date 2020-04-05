1f47b0fc61c0bfc8d34e4644cf9bcf76119e9b7c
Mattssu <shednic55@gmail.com>
2020-04-05 15:49:11 +0300

    + exported parser to own file

--- modules_parser.py
+++ modules_parser.py
@@ -5,6 +5,7 @@ from Decoders.ASCIICipher import ASCIIDecoder
 from Decoders.Base64Cipher import Base64Decoder
 from Decoders.CaesarCipher import CaesarDecoder
 from Decoders.ReverseCipher import ReverseDecoder
+from personal_parser import MyParser
 from evaluator import evaluate
 import argparse
 
@@ -24,40 +25,6 @@ DECODERS_MAP = {'ascii': ASCIIDecoder,
 
 
 ###########################
-
-class MyParser(argparse.ArgumentParser):
-    def __init__(self,
-                 prog=None,
-                 usage=None,
-                 description=None,
-                 epilog=None,
-                 parents=[],
-                 formatter_class=argparse.HelpFormatter,
-                 prefix_chars='-',
-                 fromfile_prefix_chars=None,
-                 argument_default=None,
-                 conflict_handler='error',
-                 add_help=True,
-                 allow_abbrev=True):
-        super().__init__(prog, usage, description, epilog, parents, formatter_class, prefix_chars,
-                         fromfile_prefix_chars,
-                         argument_default, conflict_handler, add_help, allow_abbrev)
-        self.problem = False
-
-    # the default status on the parent class is 0, we're
-    # changing it to be 1 here ...
-    def exit(self, status=1, message=None):
-        if message:
-            self._print_message(message, sys.stderr)
-        self.problem = True
-        return
-
-
-    def parse_args(self, args=None, namespace=None):
-        if self.parse_known_args(args, namespace) == None:
-            return
-        else:
-            super().parse_args(args,namespace)
 
 
 ###########################
---
+++ personal_parser.py
@@ -0,0 +1,35 @@
+import argparse
+
+
+class MyParser(argparse.ArgumentParser):
+    def __init__(self,
+                 prog=None,
+                 usage=None,
+                 description=None,
+                 epilog=None,
+                 parents=[],
+                 formatter_class=argparse.HelpFormatter,
+                 prefix_chars='-',
+                 fromfile_prefix_chars=None,
+                 argument_default=None,
+                 conflict_handler='error',
+                 add_help=True,
+                 allow_abbrev=True):
+        super().__init__(prog, usage, description, epilog, parents, formatter_class, prefix_chars,
+                         fromfile_prefix_chars,
+                         argument_default, conflict_handler, add_help, allow_abbrev)
+        self.problem = False
+
+    # the default status on the parent class is 0, we're
+    # changing it to be 1 here ...
+    def exit(self, status=1, message=None):
+        if message:
+            self._print_message(message, sys.stderr)
+        self.problem = True
+        return
+
+    def parse_args(self, args=None, namespace=None):
+        if self.parse_known_args(args, namespace) is None:
+            return
+        else:
+            super().parse_args(args, namespace)
