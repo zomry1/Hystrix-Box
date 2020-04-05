010e214f3f3c69079cd9999a83fdd2afbc32c233
Mattssu <shednic55@gmail.com>
2020-04-05 22:01:13 +0300

    + fixed problem in L33T translator

--- Utils/LeetCipher.py
+++ Utils/LeetCipher.py
@@ -12,23 +12,22 @@
 
 
 def fromLeet(text):
-    leet_dict = (
-        ('r', 'are'),
-        ('8', 'ate'),
-        ('m8', 'mate'),
-        ('tht', 'that'),
-        ('j00', 'you'),
-        ('0', 'o'),
-        ('1', 'i'),
-        ('3', 'e'),
-        ('5', 's'),
-        ('4', 'a'),
-        ('7', 't'),
-    )
-    for symbols, replaceStr in leet_dict:
-        for symbol in symbols:
-            text = text.replace(symbol, replaceStr)
-    return text
+    leet_dict = {"8": "ate",
+                 'm8': 'mate',
+                 'tht': "that",
+                 'j00': "you",
+                 '0': 'o',
+                 '1': 'i',
+                 '3': 'e',
+                 '5': 's',
+                 '4': 'a',
+                 '7': 't', }
+
+    temp_str = text
+    for replaced_char in temp_str:
+        if replaced_char in leet_dict:
+            temp_str = temp_str.replace(replaced_char, leet_dict[replaced_char])
+    return temp_str.lower()
 
 # message = input("Please enter a message: ")
-# print("Translated Message: ", fromLeet(message).lower())
+# print("Translated Message: ", fromLeet(message))
