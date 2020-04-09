9051ffb912a1274440f282534a363a760fba7da9
zomry1 <zomry1@gmail.com>
2020-04-10 02:18:25 +0300

    Add requirments and travis file

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from distutils.core import setup
+
+with open('README.md', 'r') as fh:
+	long_description = fh.read()
+
 setup(
   name = 'Hystrix-Box',         # How you named your package folder (MyLib)
   packages = ['Hystrix-Box'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Ultimate toolbox for solving CTF challenges',   # Give a short description about your library
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   author = 'zomry1',                   # Type in your name
   author_email = 'zomry1@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/zomry1',   # Provide either the link to your github or to your website
+  url = 'https://github.com/zomry1/Hystrix-Box/',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['CTF', 'toolbox', 'challenges'],   # Keywords that define your package best
+  packages=setuptools.find_packages(),
   install_requires=[            # I get to this in a second
           'validators',
           'beautifulsoup4', 'requests'
