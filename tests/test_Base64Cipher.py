d3b3fdb0c8477c329024c9a2f41d48b4d1cd4816
zomry1 <zomry1@gmail.com>
2020-04-10 03:01:27 +0300

    Add tests
    tests for base64 decoder

---
+++ tests/test_Base64Cipher.py
@@ -0,0 +1,21 @@
+from Decoders.Base64Cipher import Base64Decoder
+
+
+def test_validate_true():
+    assert (Base64Decoder.validate('dGhpcyBpcyBhIHRlc3Q=') == True)
+
+
+def test_validate_false():
+    assert (Base64Decoder.validate('dGhpcyBpcyBhIHRlc3Q==') == False)
+
+
+def test_decode_true():
+    assert (Base64Decoder.decode('dGhpcyBpcyBhIHRlc3Q=') == ['this is a test'])
+
+
+def test_safe_decode_true():
+    assert (Base64Decoder.safe_decode('dGhpcyBpcyBhIHRlc3Q=') == ['this is a test'])
+
+
+def test_safe_decode_noFormat():
+    assert (Base64Decoder.safe_decode('dGhpcyBpcyBhIHRlc3Q==') == [])
