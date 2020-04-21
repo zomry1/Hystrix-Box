b55f97e97b4c6f7ecdc96b906a42a4d611e1d489
zomry1 <zomry1@gmail.com>
2020-04-21 11:53:09 +0300

    🔥 Remove word analysis

--- HystrixBox/keys.py
+++ HystrixBox/keys.py
@@ -1,5 +1,5 @@
-import os
+#import os
 
 # For word analysis - https://developer.oxforddictionaries.com/
-APP_ID = os.environ['OXFORD_APP_ID']
-APP_KEY = os.environ['OXFORD_APP_KEY']
+APP_ID = 'Not in use' #os.environ['OXFORD_APP_ID']
+APP_KEY = 'Not in use' #os.environ['OXFORD_APP_KEY']
